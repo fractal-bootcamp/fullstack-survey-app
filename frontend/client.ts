1a4824a5ef9cbfed917eac6c39ff61b7dea73bd4
dxren <dorothy.x.ren@gmail.com>
2025-02-07 15:37:15 -0500

    update

--- frontend/client.ts
+++ frontend/client.ts
@@ -1,6 +1,6 @@
 import { treaty } from '@elysiajs/eden'
 import type { ApiApp } from '../index'
 
-const apiClient = treaty<ApiApp>('localhost:3000')
+const apiClient = treaty<ApiApp>(import.meta.env.VITE_SERVER_URL)
 
 export default apiClient
